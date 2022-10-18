8a62853f4ea84757013c6e4d83037e5f880da6a4
RadhaGulhane13 <radhagulhane1300@gmail.com>
2022-10-18 12:21:59 -0400

    Update the code: - Include functions for modularization - Support for reading input and writing output using files

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,8 @@
-import java.io.DataInputStream;
-import java.io.DataOutputStream;
-import java.io.IOException;
+import java.io.*;
 import java.net.InetAddress;
 import java.net.ServerSocket;
 import java.net.Socket;
-import java.util.HashMap;
-import java.util.LinkedList;
-import java.util.Queue;
+import java.util.*;
 import java.util.concurrent.Semaphore;
 
 
@@ -57,10 +53,26 @@ class ProcessHandler {
                 String req_timestamp=  client_input.readUTF();
                 String req_process_name =  client_input.readUTF();
 
+                /*
+                boolean res = true;
+
+                if (critical_section_access == true)
+                res = false;
+                else if (critical_section_request == true) {
+                if (Integer.parseInt(this.curr_proc_req_timestamp) >  Integer. parseInt(req_timestamp))
+                res = false;
+                else if(Integer.parseInt(this.curr_proc_req_timestamp) ==  Integer. parseInt(req_timestamp))
+                if (req_process_name.compareTo(this.process_name) > 0) {
+                System.out.println("compare"+req_process_name+"to"+this.process_name+": "+req_process_name.compareTo(this.process_name));
+                res = false;
+                }
+
+                }*/
+
                 boolean res = critical_section_access == false &&
                         (critical_section_request == false ||
                                 (critical_section_request == true && Integer. parseInt(this.curr_proc_req_timestamp) >  Integer. parseInt(req_timestamp)));
-                System.out.println("Process "+this.process_name+"Receives req from "+req_process_name+ "res : "+res);
+//                System.out.println("Process "+this.process_name+"Receives req from "+req_process_name+ "res : "+res);
                 if (res) {
 //                                ((Integer. parseInt(this.curr_proc_req_timestamp) ==  Integer. parseInt(req_timestamp) && req_process_name.compareTo(this.process_name) < 0) ||
 //                                Integer. parseInt(this.curr_proc_req_timestamp) >  Integer. parseInt(req_timestamp)))))
@@ -87,43 +99,6 @@ class ProcessHandler {
         this.other_processess = new ProcessHandler[2];
         this.other_processess[0] = p1;
         this.other_processess[1] = p2;
-    }
-
-
-    /* Process Handler Server */
-    public void recv_req(String req_timestamp, String req_process_name) throws IOException, InterruptedException {
-        mutex.acquire();
-        Socket socket = server.accept();
-        DataInputStream client_input = new DataInputStream(socket.getInputStream());
-        DataOutputStream client_output = new DataOutputStream(socket.getOutputStream());
-
-
-        System.out.println("Requ from :"+req_process_name+"to"+this.process_name);
-        boolean res = critical_section_access == false &&
-                (critical_section_request == false ||
-                        (critical_section_request == true && Integer. parseInt(this.curr_proc_req_timestamp) >  Integer. parseInt(req_timestamp)));
-        System.out.println("Process "+this.process_name+"Receives req from "+req_process_name+ "res : "+res);
-        if (res) {
-//                                ((Integer. parseInt(this.curr_proc_req_timestamp) ==  Integer. parseInt(req_timestamp) && req_process_name.compareTo(this.process_name) < 0) ||
-//                                Integer. parseInt(this.curr_proc_req_timestamp) >  Integer. parseInt(req_timestamp)))))
-            System.out.println("Process " + this.process_name + "send REPLY to" + req_process_name+"port :"+this.port);
-            client_output.writeUTF("REPLY from " + this.port+"to "+req_process_name);
-
-        } else {
-            /* else put the request in queue */
-
-            deferred_req.add(socket);
-            //deferred_req.add(client_output);
-            System.out.println("Process "+this.process_name+"Defered req "+req_process_name);
-            //     System.out.println("Defered prcess " + req_process_name+"by"+this.process_name+ "param :" +this.process_name+":"+this.curr_proc_req_timestamp +"  "+req_process_name+" :"+req_timestamp);
-        }
-        // mutex.release();
-
-        /* get process_clock_timestamp and other information related to transaction */
-        /* check if ricart can send the REPLY */
-        /* Send REPLY if possible */
-        /* else put the request in queue*/
-
     }
 
     public void set_timestamp(String req_timestamp) throws InterruptedException {
@@ -193,14 +168,18 @@ class ProcessHandler {
         mutex.acquire();
         critical_section_access = true;
         mutex.release();
-        /* mutex update request_timestamp = clock_timestamp */
-        /* wait the REPLY from everyone*/
+
+
 
         /* Access critical section */
         String output;
         switch (transaction) {
             case "CheckBalance":
                 System.out.println("**********CheckBalance : "+user + " has $"+this.database.get(user)+transac);
+                String write_file_name = "output.txt";
+                BufferedWriter wr = new BufferedWriter(new FileWriter(write_file_name, true));
+                wr.write(user + " has $"+this.database.get(user)+"\n");
+                wr.close();
                 break;
             case "DepositCash":
                 this.database.put(user, this.database.get(user) + Integer.valueOf(value));
@@ -219,14 +198,6 @@ class ProcessHandler {
                 System.out.println("**************** INVALID TRANSACTION COMMAND ******************"+transac);
 
         }
-        /* send request to other two processess*/
-//        (new Thread() {
-//            public void run() {
-//                /**/
-//            }
-//        }).start();
-        /* ACCESS CRITICAL SECTION */
-        /* send REPLY to all defereed process*/
 
         mutex.acquire();
         if (!req_timestamp_queue.isEmpty())
@@ -242,84 +213,95 @@ class ProcessHandler {
         while(!deferred_req.isEmpty()) {
             Socket socket =deferred_req.remove();
             DataOutputStream client_output = new DataOutputStream(socket.getOutputStream());
-//            DataOutputStream client_output = deferred_req.remove();
             client_output.writeUTF("REPLY2 from "+this.port);
         }
-        System.out.println("process :" + p_name+"\n");
 
         process_exe.release();
     }
-
-//    public void server_start() throws IOException {
-//        server = new ServerSocket(port);
-//    }
-
 }
-/*
-class ThreadHandler extends Thread {
-    ProcessHandler process_handler;
-    String process_id;
-    String timestamp;
+
+class Transcation {
+    String processID;
     String transaction;
-    String user, String value
+    String timestamp;
+    String user;
+    String value;
+    String operation;
 
-    public ThreadHandler(ProcessHandler process_handler, String process_id, String timestamp, String transaction, String user, String value) {
-        this.process_handler = process_handler;
-        this.process_id = process_id;
-        this.timestamp = timestamp;
-        String transaction, String user, String value
-    }
-
-    public void run()
+    public void initialize(String processID, String transaction, String timestamp, String user, String value, String operation)
     {
-        this.process_handler.exec_req(this.process_id);
+        this.processID = processID;
+        this.transaction = transaction;
+        this.timestamp = timestamp;
+        this.user = user;
+        this.value = value;
+        this.operation = operation;
     }
 }
-*/
-
 
 public class Main {
+    static HashMap<String, Integer> database;
+    static ProcessHandler[] processes;
+    static HashMap<String, ProcessHandler> map;
+    static Semaphore req_exe = new Semaphore(1);
+    static int clock_var =0;
+    String input_transactions[];
+    static List<Transcation> T = new ArrayList<Transcation>();
 
+    public static Comparator<Transcation> PriorityComparator
+            = new Comparator<Transcation>() {
 
-    public static void main(String[] args) throws IOException, InterruptedException {
-//        String t = "P1: CheckBalance (1, A)".replaceAll(" ", "");
-//
-//        String temp[] = t.split("[:,()]");
-//
-//        for (int i =0; i < temp.length;i++)
-//            System.out.println(temp[i] + " "+temp.length);
+        public int compare(Transcation t1, Transcation t2) {
+            if (Integer.parseInt(t1.timestamp) == Integer.parseInt(t2.timestamp)) {
+                return t1.processID.compareTo(t2.processID);
+            } else
+                return (Integer.parseInt(t1.timestamp) < Integer.parseInt(t2.timestamp)) ? -1 : 1;
+        }
 
-        Integer clock_var = 0;
+    };
+    /* Receives operations at particular timestamp */
+    public static void processScheduler(Transcation trans) throws InterruptedException {
+        req_exe.acquire();
+        System.out.println("set timestamp for "+trans.processID + "to "+trans.timestamp);
+        //map.get(trans.processID).set_timestamp(trans.timestamp);
 
-        Integer processes = 3;
-        Semaphore req_exe = new Semaphore(1);
-        /*
-        P1: DepositCash (4, A, $20), ApplyInterest (57, C, 10%), CheckBalance (200, A)
-– P2: WithdrawCash (4, C, $30), DepositCash (63, B, $40), CheckBalance (200, B)
-– P3: ApplyInterest (2, B, 10%), WithrawCash (68, A, $10), CheckBalance (200, C) */
+        map.get(trans.processID).set_timestamp(String.valueOf(clock_var));
+        clock_var++;
+        req_exe.release();
+        System.out.println("curr_p : "+ trans.processID);
 
-//        String Transaction_P1 = "P1: DepositCash (4, A, $20), ApplyInterest (57, C, 10%), CheckBalance (200, A)";
-//        String Transaction_P2 = "P2: WithdrawCash (4, C, $30), DepositCash (63, B, $40), CheckBalance (200, B)";
-//
-//        String Transaction_P3 = "P3: ApplyInterest (2, B, 10%), WithrawCash (68, A, $10), CheckBalance (200, C)";
+        new Thread() {
+            public void run() {
+                try {
+                    map.get(trans.processID).exec_req(trans.processID,
+                            trans.transaction, trans.user,trans.value, trans.operation);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }.start();
 
+    }
 
-//        String sorted_transactions[] = {"P3: ApplyInterest (2, B, 10%)", "P1: DepositCash (4, A, $20)",
-//                                        "P2: WithdrawCash (4, C, $30)"};
-        // String sorted_transactions[] = {"P1: CheckBalance (1, A)", "P2: CheckBalance (10, B)", "P3: CheckBalance (100, C)"};
-        //String sorted_transactions[] = {"P3: ApplyInterest (2, B, 10%)", "P1: DepositCash (4, A, $20)"};
+    public static void TransactionHandler() throws InterruptedException {
 
-        String sorted_transactions[] = {"P3: ApplyInterest (2, B, 10)","P1: DepositCash (4, A, 20)", "P2: WithdrawCash (4, C, 30)", "P1: ApplyInterest (57, C, 10)","P2: DepositCash (63, B, 40)", "P3: WithdrawCash (68, A, 10)", "P1: CheckBalance (1, A)", "P2: CheckBalance (10, B)", "P3: CheckBalance (100, C)"};
-        //    String sorted_transactions[] = {"P3: ApplyInterest (2, B, 10)","P1: DepositCash (4, A, 20)", "P2: WithdrawCash (4, C, 30)"};
-        HashMap<Integer, String[]> timestamp_str = new HashMap<>();
-        timestamp_str.put(2, new String[]{"P3: ApplyInterest (2, B, 10)"});
-        timestamp_str.put(4, new String[]{"P1: DepositCash (4, A, 20)", "P2: WithdrawCash (4, C, 30)"});
+        for (int i = 0; i < T.toArray().length; i++) {
+            processScheduler(T.get(i));
+        }
+    }
 
-
-        HashMap<String, Integer> database = new HashMap<>();
+    public static void databaseInitialization() {
+        database = new HashMap<>();
         database.put("A", 100);
         database.put("B", 100);
         database.put("C", 100);
+    }
+
+    public static void processInitialization() throws IOException {
+
+        processes = new ProcessHandler[3];
 
         ProcessHandler p1 = new ProcessHandler(5001, "P1", database);
         ProcessHandler p2 = new ProcessHandler(5002, "P2", database);
@@ -329,146 +311,79 @@ public class Main {
         p2.assign_other_processes(p1, p3);
         p3.assign_other_processes(p1, p2);
 
-        /* Run server */
-        new Thread() {
-            public void run() {
+        processes[0] = p1;
+        processes[1] = p2;
+        processes[2] = p3;
+        /* Start  server: TBD: move this in constructor */
+        for (int i = 0; i < processes.length; i++) {
+            /* Run server */
 
-
-                try {
-                    p1.server_on();
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-            }
-        }.start();
-
-        new Thread() {
-            public void run() {
-
-
-                try {
-                    p2.server_on();
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-            }
-        }.start();
-
-        new Thread() {
-            public void run() {
-
-
-                try {
-                    p3.server_on();
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-            }
-        }.start();
-
-        HashMap<String, ProcessHandler> map = new HashMap<>();
-        map.put("P1", p1);
-        map.put("P2", p2);
-        map.put("P3", p3);
-
-        /* first sort all processes and at each clock check how many processes are there, accord*/
-        int curr_tranc_idx = 0;
-
-        for (int i = 0; i < sorted_transactions.length; i++) {
-            String trans = sorted_transactions[i];
-            String ele[] = sorted_transactions[i].replaceAll(" ", "").split("[:,()]");
-            String process_id = ele[0];
-            String transaction = ele[1];
-            String timestamp = ele[2];
-            String user = ele[3];
-
-            req_exe.acquire();
-            map.get(ele[0]).set_timestamp(clock_var.toString());
-            clock_var++;
-            req_exe.release();
-            System.out.println("curr_p : "+ ele[0]);
-
-            String value = "";
-            if (ele.length == 5)
-                value =  ele[4];
-
-            String finalValue = value;
+            int finalI = i;
             new Thread() {
                 public void run() {
-
-
                     try {
-                        map.get(ele[0]).exec_req(process_id, transaction, user, finalValue, trans);
-                    } catch (InterruptedException e) {
-                        e.printStackTrace();
+                        processes[finalI].server_on();
                     } catch (IOException e) {
                         e.printStackTrace();
                     }
                 }
             }.start();
         }
-/*
-            if (ele[0].equals("P1")) {
-               // System.out.println(ele[1] + " : "+ele[2] + " : "+ele[3] + "\n");
-//                new ThreadHandler(p1, process_id, timestamp, transaction, user, value).start();
-                new Thread() {
-                    public void run() {
+
+        map = new HashMap<>();
+        map.put("P1", p1);
+        map.put("P2", p2);
+        map.put("P3", p3);
 
 
-                        try {
-                            p1.exec_req(process_id, timestamp, transaction, user, value);
-                        } catch (InterruptedException e) {
-                            e.printStackTrace();
-                        } catch (IOException e) {
-                            e.printStackTrace();
-                        }
-                    }
-                }.start();
+    }
 
-            } else if (ele[0].equals("P2")) {
-                //System.out.println(ele[1] + ele[2] + ele[3] + "\n");
-                //new ThreadHandler(p2, process_id, timestamp, transaction, user, value).start();
-                new Thread() {
-                    public void run() {
+    public static void main(String[] args) throws IOException, InterruptedException {
 
-                        try {
-                            p2.exec_req(process_id, timestamp, transaction, user, value);
-                        } catch (InterruptedException e) {
-                            e.printStackTrace();
-                        } catch (IOException e) {
-                            e.printStackTrace();
-                        }
+        readTransaction();
+        databaseInitialization();
+        processInitialization();
+        TransactionHandler();
+    }
 
-                    }
-                }.start();
+    private static void readTransaction() throws IOException {
 
-            } else if (ele[0].equals("P3")) {
-               // System.out.println(ele[1] + ele[2] + ele[3] + "\n");
-              //  new ThreadHandler(p3, process_id, timestamp, transaction, user, value).start();
-                new Thread() {
-                    public void run() {
-
-                        try {
-                            p3.exec_req(process_id, timestamp, transaction, user, value);
-                        } catch (InterruptedException e) {
-                            e.printStackTrace();
-                        } catch (IOException e) {
-                            e.printStackTrace();
-                        }
+        String read_file_name = "input.txt";
+        BufferedReader br = new BufferedReader(new FileReader(read_file_name));
 
 
-                    }
-                }.start();
+        String data;
 
-            } else {
-                // invalid prcess id
+
+        while((data = br.readLine()) != null) {
+            String processID = data.substring(0, 2);
+            String transactions_by_process[] = data.substring(3, data.length()).replaceAll(" ", "").split("\\),");
+
+
+            for (int i = 0; i < transactions_by_process.length;i++) {
+                Transcation t = new Transcation();
+                t.processID =processID;
+                String transac[] = transactions_by_process[i].split("[(,)]");
+
+                t.transaction = transac[0];
+                t.timestamp = transac[1];
+                t.user = transac[2];
+                t.operation = transactions_by_process[i];
+                if (transac.length == 4) {
+                    t.value = transac[3].replace("$", "").replace("%", "");
+                } else
+                    t.value = "";
+                T.add(t);
             }
+        };
+        T.sort(PriorityComparator);
+
+        for(int k = 0; k <T.toArray().length;k++)
+        {
+            Transcation temp = T.get(k);
+            System.out.println(temp.processID+ " "+temp.timestamp+" "+temp.transaction+" "+temp.user+" " +temp.value);
 
         }
-        */
-        //  Thread.sleep(2000); /*MAINTAIN THE CLOCK CYCLE*/
-        //ThreadHandler myThreads[] = new ThreadHandler[processes];
-
     }
 
 
